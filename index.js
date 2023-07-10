5b0f5d8da6fd96fa239483763313b5ca89245fa5
Andrew <andrew.i.funke@gmail.com>
2023-07-10 14:03:10 -0700

    Using Array Reduce Lab

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = 0;
 
 // Code your solution here
+function Reducer (accumulator,currentValue) {
+    batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, totalBatteries);
+}
+
+totalBatteries  = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, totalBatteries);
+
+console.log(totalBatteries);
